9995416049c0d52442d264fd39a49ca05286bdee
Josue Estrada <josuegabrielestrada@gmail.com>
2022-04-20 17:43:31 -0400

    added mongoDB connection

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-import flask
+from flask import Flask
+import pymongo
+import os
+
+app = Flask(__name__)
+# MongoDB
+client = pymongo.MongoClient("mongodb+srv://admin:" + os.environ["MONGO_APPOINTMED_PWD"] +
+                             "@cluster0.vsmni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
