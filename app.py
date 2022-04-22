a4fca0ad6afa0efddeb4f53d07115f802bc55940
Josue Estrada <josuegabrielestrada@gmail.com>
2022-04-22 15:50:58 -0400

    added types to parameters

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from django.shortcuts import redirect
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 import pymongo
 import os
 
-
-
 app = Flask(__name__)
-# MongoDB
+# Josue MongoDB
 # client = pymongo.MongoClient("mongodb+srv://admin:" + os.environ["MONGO_APPOINTMED_PWD"] +
 #                              "@cluster0.vsmni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-client = pymongo.MongoClient( "mongodb+srv://SDS:" + os.environ.get("MONGO_PW")+ "@cluster0.zc52h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
+client = pymongo.MongoClient("mongodb+srv://SDS:" + os.environ.get(
+    "MONGO_PW") + "@cluster0.zc52h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 db = client.test
 time_slots = ["8:00am", "8:30am", "9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm", "12:30pm",
@@ -25,16 +24,17 @@ def home():
 
 @app.route("/Schedule", methods=["GET", "POST"])
 def schedule():
-    day=''
+    day = ''
     if request.form == 'POST':
-        day=request.form['day']
-        return redirect("/"+day)
+        day = request.form['day']
+        return redirect("/" + day)
     else:
-        return render_template("appointment.html",day=day,time_slots=time_slots)
+        return render_template("appointment.html", day=day, time_slots=time_slots)
+
+
 @app.route("/Schedule/<day>", methods=["GET", "POST"])
 def scheduleTime(day):
-    return render_template("appointment.html",day=day,time_slots=time_slots)
-
+    return render_template("appointment.html", day=day, time_slots=time_slots)
 
 
 @app.route("/seed_db")
