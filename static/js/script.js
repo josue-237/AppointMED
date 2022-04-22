e1f7397313a4e5ede4dca3be4c104e06891a3004
Linerah <kevin.linera@upr.edu>
2022-04-21 23:54:55 -0400

    added doctor search and map to the home page

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 	18.200178, lng: -66.44513},
+        zoom: 10,
+        mapId: '122517338e1e4a8'
+        });
+}
