0337ebe9a7662d752b960685ee46b45f5b5bff47
Josue Estrada <josuegabrielestrada@gmail.com>
2022-04-23 13:54:32 -0400

    fixed param refactor issue

--- event.py
+++ event.py
@@ -7,22 +7,28 @@ class Event:
         :param appointment_id: ID of appointment associated with event
         :param doctor_id: ID of doctor associated with event
         """
-
-        self.start_date = self.set_start_time(start_time)
-        self.end_date = self.set_date(date)
+        self.date = self.set_date(date)
+        self.start_time = self.set_start_time(start_time)
         self.appointment_id = self.set_appointment_id(appointment_id)
         self.doctor_id = self.set_doctor_id(doctor_id)
 
-    # Setters
-    def set_start_time(self, start_time: str):
-        if type(start_time) != str:
-            raise TypeError("start_time must be a string")
-        self.start_date = start_time
+    def __str__(self):
+        """
+        Method returns a string representation of the Event object
+        :return: String representation of Event object
+        """
+        return f"Event: start_date: {self.date}, {self.start_time}, {self.appointment_id}, {self.doctor_id}"
 
+    # Setters
     def set_date(self, date: str):
         if type(date) != str:
             raise TypeError("date must be a string")
-        self.end_date = date
+        self.date = date
+
+    def set_start_time(self, start_time: str):
+        if type(start_time) != str:
+            raise TypeError("start_time must be a string")
+        self.start_time = start_time
 
     def set_appointment_id(self, appointment_id: str):
         if type(appointment_id) != str:
@@ -35,11 +41,11 @@ class Event:
         self.doctor_id = doctor_id
 
     # Getters
-    def get_start_time(self):
-        return self.start_date
-
     def get_date(self):
-        return self.end_date
+        return self.date
+
+    def get_start_time(self):
+        return self.start_time
 
     def get_appointment_id(self):
         return self.appointment_id
@@ -54,8 +60,8 @@ class Event:
         :return: dictionary of properties and class values
         """
         return {
-            "start_time": self.start_date,
-            "date": self.end_date,
+            "date": self.date,
+            "start_time": self.start_time,
             "appointment_id": self.appointment_id,
             "doctor_id": self.doctor_id
         }
@@ -71,7 +77,7 @@ class Event:
         :param database: MongoDB database
         :return: Event object
         """
-        event = Event(start_time, date, appointment_id, doctor_id)
+        event = Event(date, start_time, appointment_id, doctor_id)
         event_document = event.to_json()
         collection = database.db.events
         collection.insert_one(event_document)
@@ -87,5 +93,5 @@ class Event:
         """
         collection = database.db.events
         event_document = collection.find_one({"appointment_id": appointment_id})
-        return Event(event_document["start_time"], event_document["date"], event_document["appointment_id"],
+        return Event(event_document["date"], event_document["start_time"], event_document["appointment_id"],
                      event_document["doctor_id"])
