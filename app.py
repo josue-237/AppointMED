74d3ad830d513dac21f80349d2a2d8b6d5ee2b68
victorvegaward <victorandresvega@gmail.com>
2022-04-23 14:21:19 -0400

    made changes to appointment class

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from appointment import Appointment
 from flask_pymongo import PyMongo
 import certifi
-
+from event import Event
 app = Flask(__name__)
 # MongoDB
 # client = pymongo.MongoClient("mongodb+srv://admin:" + os.environ["MONGO_APPOINTMED_PWD"] +
@@ -22,7 +22,7 @@ app.config[
 time_slots = ["08:00am", "08:30am", "09:00am", "09:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm",
               "12:30pm", "01:00pm", "01:30pm", "02:00pm", "02:30pm", "03:00pm", "03:30pm", "04:00pm", "04:30pm"]
 
-doctor_ids = {"Richard Silverstein": 1234}
+doctor_ids = {"Richard Silverstein": "1234"}
 
 mongo = PyMongo(app, tlsCAFile=certifi.where())
 db = client.test
@@ -41,21 +41,33 @@ def schedule():
     medical_plans = ["Triple S", "Medicaid", "UnitedHealth"]
     doctor_name = "Richard Silverstein"
     doc_id = doctor_ids[doctor_name]
+    
     if request.form == "GET":
-
+        time_slots2=[]
+        time=''
+        
         # time_slots= Appointment.get_available_time_slots(doc_id,"04-22-2022",mongo)
-        return render_template("appointment.html", day='', time_slots=time_slots,
+        return render_template("appointment.html", day='', time_slots=time_slots2,
                                doctor_image="https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg",
                                doctor_name="Richard Silverstein", doctor_specialty="Dermatologist",
                                doctor_address="14 Calle Peral N Ste La Mayaguez PR, 00680, Estados Unidos",
                                doctor_phone="559-206-4429", medical_plans=medical_plans)
     else:
         day = request.form.get('day')
-        return render_template("appointment.html", day=day, time_slots=time_slots,
+        time=''
+        time=request.form.get('time')
+        appt_id=Appointment.generate_appointment_id()
+        collection = mongo.db.events
+        time_slots2=Appointment.get_available_time_slots(doc_id,day,mongo,time_slots)
+        if time:
+            return redirect("/Schedule/"+appt_id)
+        return render_template("appointment.html", day=day, time_slots=time_slots2,
                                doctor_image="https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg",
                                doctor_name="Richard Silverstein", doctor_specialty="Dermatologist",
                                doctor_address="14 Calle Peral N Ste La Mayaguez PR, 00680, Estados Unidos",
                                doctor_phone="559-206-4429", medical_plans=medical_plans)
+    # elif request.form == "POST"  and time:
+    #     return redirect('/Schedule/'+time)
 
 
 @app.route("/datepicker")
@@ -65,4 +77,36 @@ def datepicker():
 
 @app.route("/seed_db")
 def seed_db():
-    pass
+    # collection = mongo.db.events
+    # collection.remove({})
+    doc_id=doctor_ids["Richard Silverstein"]
+    collection = mongo.db.events
+    event1=Event("22-06-2022","08:30am","2423fe323",doc_id)
+    event1json=to_json("08:30am","04/25/2022","2423fe323",doc_id)
+    collection.insert(event1json)
+    event2json=to_json("09:30am","04/25/2022","2423fe323",doc_id)
+    collection.insert(event2json)
+    event3json=to_json("10:30am","04/25/2022","2423fe323",doc_id)
+    collection.insert(event3json)
+    event4json=to_json("11:30am","04/25/2022","2423fe323",doc_id)
+    collection.insert(event4json)
+    event5json=to_json("01:30pm","04/25/2022","2423fe323",doc_id)
+    collection.insert(event5json)
+    # event2=Event.create_event("22-06-2022","09:30am","2423fe323",doc_id,mongo)
+    # event3=Event.create_event("22-06-2022","10:30am","2423fe323",doc_id,mongo)
+    # event4=Event.create_event("22-06-2022","11:30am","2423fe323",doc_id,mongo)
+    # event5=Event.create_event("22-06-2022","01:30pm","2423fe323",doc_id,mongo)
+    
+    return "seeded successfully"
+  
+def to_json(start_time,date,appointment_id,doc_id):
+        """
+        Method returns every class property as a JSON
+        :return: dictionary of properties and class values
+        """
+        return {
+            "start_time": start_time,
+            "date": date,
+            "appointment_id": appointment_id,
+            "doctor_id": doc_id
+        }
--- appointment.py
+++ appointment.py
@@ -1,3 +1,6 @@
+import copy
+import time 
+import hashlib
 class Appointment:
     def __init__(self,doctor_name,doctor_image,phone,address,specialty, appointment_id, doctor_id):
         """
@@ -12,7 +15,7 @@ class Appointment:
         """
 
         self.name=self.set_doctor_name(doctor_name)
-        self.appointment_id = self.set_appointment_id(appointment_id)
+        self.appointment_id = self.generate_appointment_id(appointment_id)
         self.doctor_id = self.set_doctor_id(doctor_id)
         self.doctor_image=self.set_doctor_image(doctor_image)
         self.address=self.set_doctor_address(address)
@@ -21,11 +24,6 @@ class Appointment:
 
     # Setters
     
-
-    def set_appointment_id(self, appointment_id):
-        if type(appointment_id) != str:
-            raise TypeError("appointment_id must be a string")
-        self.appointment_id = appointment_id
     def set_doctor_id(self, doctor_id):
         if type(doctor_id) != str:
             raise TypeError("doctor_id must be a string")
@@ -53,11 +51,18 @@ class Appointment:
     @staticmethod
     def get_available_time_slots(doc_id,day,database,time_slots):
         collection = database.db.events
-        time_slots_filled=list(collection.find({"$and":[ {"doc_id":doc_id}, {"day":day}]}))
+        # result=time_slots
+        result=copy.deepcopy(time_slots)
+        time_slots_filled=collection.find({"$and":[ {"doctor_id":doc_id}, {"date":day}]})
         for i in time_slots_filled:
-            time_slots=time_slots.remove(i)
-        return time_slots
+            result.remove(i["start_time"])
+        return result
     def to_json(self):
         return {'username': self.username, 'ratings': self.ratings , 'raters_amount': self.raters_amount, 'user_items': self.user_items, 'profile_image': self.profile_image}
-
-    
+    @staticmethod
+    def generate_appointment_id():
+        # Generate unique user ID
+        cur_time = str(time.time())
+        hashed_time = hashlib.sha1()
+        hashed_time.update(cur_time.encode("utf8"))
+        return hashed_time.hexdigest()
