4f06fe3ffc8379eb91d9ecd4a0df9113e7c73773
victorvegaward <victorandresvega@gmail.com>
2022-04-24 12:25:18 -0400

    fixed bugs

--- appointment.py
+++ appointment.py
@@ -13,7 +13,7 @@ class Appointment:
         :param phone: Phone to contact doctor's office
         """
 
-        self.name=doctor_name
+        self.name=self.set_doctor_name(doctor_name)
         self.doctor_id = self.set_doctor_id(doctor_id)
         self.doctor_image=self.set_doctor_image(doctor_image)
         self.address=self.set_doctor_address(address)
@@ -25,22 +25,22 @@ class Appointment:
     def set_doctor_id(self, doctor_id):
         if type(doctor_id) != str:
             raise TypeError("doctor_id must be a string")
-        self.appointment_id = doctor_id
+        self.doctor_id = doctor_id
         return doctor_id
     def set_doctor_specialty(self, specialty):
         if type(specialty) != str:
             raise TypeError("specialty must be a string")
-        self.doctor_id = specialty
+        self.specialty = specialty
         return specialty
     def set_doctor_address(self, address):
         if type(address) != str:
             raise TypeError("address must be a string")
-        self.doctor_id = address
+        self.doctor_address = address
         return address
     def set_doctor_image(self, doctor_image):
         if type(doctor_image) != str:
             raise TypeError("doctor_image must be a string")
-        self.doctor_id = doctor_image
+        self.doctor_image = doctor_image
         return doctor_image
     def set_doctor_name(self, doctor_name):
         if type(doctor_name) != str:
@@ -50,8 +50,10 @@ class Appointment:
     def set_doctor_phone(self, doctor_phone):
         if type(doctor_phone) != str:
             raise TypeError("doctor_phone must be a string")
-        self.name = doctor_phone
+        self.phone = doctor_phone
         return doctor_phone
+    
+    
     @staticmethod
     def get_available_time_slots(doc_id,day,database,time_slots):
         collection = database.db.events
@@ -61,8 +63,12 @@ class Appointment:
         for i in time_slots_filled:
             result.remove(i["start_time"])
         return result
+    
+    
     def to_json(self):
         return {'username': self.username, 'ratings': self.ratings , 'raters_amount': self.raters_amount, 'user_items': self.user_items, 'profile_image': self.profile_image}
+    
+    
     @staticmethod
     def generate_appointment_id():
         # Generate unique user ID
